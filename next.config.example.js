d01e4036f747fae50452a10d550fa0daafba2062
硬硬屎 <2481729398@qq.com>
2023-08-31 18:02:58 +0800

    Update next.config.example.js

--- next.config.example.js
+++ next.config.example.js
@@ -5,7 +5,7 @@ const nextConfig = {
     env:{
         LIVEKIT_API_KEY:"APIxxxxx",
         LIVEKIT_API_SECRET:"IssJxx",
-        LIVEKIT_WS_URL:"wss://test-xxx.livekit.cloud",
+        LIVEKIT_WS_URL:"https://chat.whstu.link/",
         UMAMI_URL:"https://xxx/script.js",
         UMAMI_ID:"xxx-xx-xx-xx-25d559c0b33d"
     }
